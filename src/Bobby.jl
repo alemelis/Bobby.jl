8cec72b05c02b9921647db09295ce7cd84ad8303
Alessandro Melis <alemelis@users.noreply.github.com>
2018-08-12 19:41:54 +0100

    fake test

--- src/Bobby.jl
+++ src/Bobby.jl
@@ -1,18 +1,10 @@
 module Bobby
 
-using FileIO
-using Compose
-using PyCall
-
-export hello, writeGame, compileGame, rmLaTeXcompilationFiles, drawBoard
+export hello
 
 function hello(name :: String)
     return "Hello $name\!"
 end
 
-include("xskak.jl")
-include("renderer.jl")
-include("pychess.jl")
-include("parser.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,4 @@ using Base.Test
 using Bobby
 
 # Run tests
-@time @test include("hello_test.jl")
-
-# @time include("skak_test.jl")
-
-@time include("renderer_test.jl")
-
-@time include("preprocess_test.jl")
+@time @test hello("Bobby") == "Hello Bobby!"
