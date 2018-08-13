b87d68dd3686c3de05407aee777c33328385a86b
Alessandro Melis <alessandro_melis@rocketmail.com>
2018-08-13 17:08:45 +0100

    first tests with bitboard representation

---
+++ src/bitboard.jl
@@ -0,0 +1,56 @@
+A = zeros(Int8, 64)
+B = zeros(Int8, 64)
+A[49:56] .= 1
+
+function intest(A, B)
+	C = zeros(Int8, 64)
+	for i=1:64
+		if A[i] != B[i]
+			C[i] = 1
+		end
+	end
+	return C
+end
+
+@benchmark intest(A, B)
+
+a = falses(64)
+b = falses(64)
+a[49:56] .= true
+
+@benchmark .~(a.|b)
+
+
+function bitest(a, b)
+	c = falses(64)
+	for i=1:64
+		if a[i] != b[i]
+			c[i] = true
+		end
+	end
+	return c
+end
+
+@benchmark bitest(a, b)
+
+function and(a, b)
+	c = falses(length(a))
+	for i=1:length(a)
+		if a[i] == b[i] & a[i] == true
+			c[i] = true
+		end
+	end
+	return c
+end
+
+function or(a, b)
+	c = falses(length(a))
+	for i=1:length(a)
+		if a[i] | b[i]
+			c[i] = true
+		end
+	end
+	return c
+end
+
+transpose(reshape(a, 8, :))
