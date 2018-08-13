d521fd3cab1ddae97f9c1f1122e5a17293a58a9e
Alessandro Melis <alemelis@users.noreply.github.com>
2018-08-13 20:28:20 +0100

    clean tests

--- test/test_bitboard.jl
+++ test/test_bitboard.jl
@@ -49,6 +49,7 @@ board = Bobby.buildBoard()
 @test all(board.B .== white_bishops)
 @test all(board.K .== white_king)
 @test all(board.Q .== white_queen)
+@test all(board.white[49:end] .== true)
 
 @test all(board.p .== black_pawns)
 @test all(board.r .== black_rooks)
@@ -56,62 +57,4 @@ board = Bobby.buildBoard()
 @test all(board.b .== black_bishops)
 @test all(board.k .== black_king)
 @test all(board.q .== black_queen)
-
-
-
-# A = zeros(Int8, 64)
-# B = zeros(Int8, 64)
-# A[49:56] .= 1
-
-# function intest(A, B)
-# 	C = zeros(Int8, 64)
-# 	for i=1:64
-# 		if A[i] != B[i]
-# 			C[i] = 1
-# 		end
-# 	end
-# 	return C
-# end
-
-# @benchmark intest(A, B)
-
-# a = falses(64)
-# b = falses(64)
-# a[49:56] .= true
-
-# @benchmark .~(a.|b)
-
-
-# function bitest(a, b)
-# 	c = falses(64)
-# 	for i=1:64
-# 		if a[i] != b[i]
-# 			c[i] = true
-# 		end
-# 	end
-# 	return c
-# end
-
-# @benchmark bitest(a, b)
-
-# function and(a, b)
-# 	c = falses(length(a))
-# 	for i=1:length(a)
-# 		if a[i] == b[i] & a[i] == true
-# 			c[i] = true
-# 		end
-# 	end
-# 	return c
-# end
-
-# function or(a, b)
-# 	c = falses(length(a))
-# 	for i=1:length(a)
-# 		if a[i] | b[i]
-# 			c[i] = true
-# 		end
-# 	end
-# 	return c
-# end
-
-# transpose(reshape(a, 8, :))
+@test all(board.black[1:16] .== true)
