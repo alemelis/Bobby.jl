feec72806eacd1d1127cce1a9df20f9b9772cdfb
Alessandro Melis <alemelis@users.noreply.github.com>
2018-12-02 21:28:14 +0000

    Pawns move and attack :boom: (#11)
    * single and doble fwd pawn moves
    Attack's not been implemented yet
    
    * Pawns attack

--- src/moves.jl
+++ src/moves.jl
@@ -171,8 +171,8 @@ function getBishopsValid(board::Bitboard, lu_tabs::Bobby.LookUpTables,
 			for j = 1:length(bishops_arr)
 				if bishops_arr[j]
 					bishops_seen += 1
-					bishops_valid[s[1]:s[2]:s[3]] .|= Bobby.slidePiece(same[s[1]:s[2]:s[3]],
-						other[s[1]:s[2]:s[3]], j)
+					bishops_valid[s[1]:s[2]:s[3]] .|= Bobby.slidePiece(
+						same[s[1]:s[2]:s[3]], other[s[1]:s[2]:s[3]], j)
 				end
 				if bishops_seen == bishops_no
 					return bishops_valid
@@ -211,8 +211,8 @@ function getQueenValid(board::Bitboard, lu_tabs::Bobby.LookUpTables,
 			queen_arr = queen[s[1]:s[2]:s[3]]
 			for j = 1:length(queen_arr)
 				if queen_arr[j]
-					queen_valid[s[1]:s[2]:s[3]] .|= Bobby.slidePiece(same[s[1]:s[2]:s[3]],
-						other[s[1]:s[2]:s[3]], j)
+					queen_valid[s[1]:s[2]:s[3]] .|= Bobby.slidePiece(
+						same[s[1]:s[2]:s[3]], other[s[1]:s[2]:s[3]], j)
 				end
 			end
 		end
@@ -249,4 +249,59 @@ function getQueenValid(board::Bitboard, lu_tabs::Bobby.LookUpTables,
 	end
 
 	return queen_valid
+end
+
+
+function getPawnsValid(board::Bitboard, lu_tabs::Bobby.LookUpTables,
+	color::String="white")
+
+	if color == "white"
+		pawns = board.P
+		other = board.black
+		increment = -8 #upward
+	elseif color == "black"
+		pawns = board.p
+		other = board.white
+		increment = +8 #downward
+	end
+
+	taken = board.taken
+
+	pawns_valid = falses(64)
+
+	for i = 1:64
+		if pawns[i]
+			# check the single space infront of the pawn
+			front_square = i + increment
+			if taken[front_square]
+				continue
+			else
+				pawns_valid[front_square] = true
+				# check double space if on home rank
+				if (increment < 0 && pawns[i] & lu_tabs.mask_rank[i,2]) || 
+					(increment > 0 && pawns[i] .& lu_tabs.mask_rank[i,7])
+					double_square = front_square + increment
+					if taken[double_square]
+						continue
+					else
+						pawns_valid[double_square] = true
+					end
+				end
+			end
+		end
+	end
+
+	# check attacking squares
+	if increment == -8
+		pawns_lx_atk = (pawns .& lu_tabs.clear_file[:,1]) << 9
+		pawns_rx_atk = (pawns .& lu_tabs.clear_file[:,8]) << 7
+	elseif increment == 8
+		pawns_lx_atk = (pawns .& lu_tabs.clear_file[:,8]) >> 7
+		pawns_rx_atk = (pawns .& lu_tabs.clear_file[:,1]) >> 9
+	end
+	pawns_attack = pawns_lx_atk .& other
+	pawns_attack .|= pawns_rx_atk .& other
+	pawns_valid .|= pawns_attack
+	
+	return pawns_valid
 end
