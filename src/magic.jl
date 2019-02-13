45538a4dd6c14abdcf0589e4292704310a5d7b57
Alessandro Melis <alemelis@users.noreply.github.com>
2019-02-13 23:20:58 +0000

    magic ranks for 1st file

--- src/magic.jl
+++ src/magic.jl
@@ -1,3 +1,47 @@
+function gen_rank_attacks()
+    rank_attacks = Dict{UInt64, Array{UInt64,1}}()
+
+    # all ranks but only the first file so far
+    for i in 1:8
+        ui = PGN2UINT[INT2PGN[1+8*(i-1)]]
+        for n in 0:2^6-1
+
+            rank_occupancy_mask_string = ("0"^8)^(i-1)*"0"
+            rank_occupancy_mask_string *= reverse(bitstring(UInt64(n))[58:end])
+            rank_occupancy_mask_string *= ("0"^8)^(8-i)
+
+            rank_occupancy_mask = cvt_to_uint(rank_occupancy_mask_string)
+            push!(rank_attacks, rank_occupancy_mask => slide_rank(
+                rank_occupancy_mask, ui, MASK_RANKS[i]))
+        end
+    end
+    return rank_attacks
+end
+RANK_ATTACKS = gen_rank_attacks()
+
+
+function slide_rank(rank_occupancy_mask::UInt64, ui::UInt64, rank_mask::UInt64)
+    direction = 1
+    increment = 1
+    moves = zeros(UInt64, 0)
+    while true
+        if (((ui >> increment) & rank_mask) != EMPTY &&
+            rank_occupancy_mask & ((ui >> increment) & rank_mask) == EMPTY)
+
+            push!(moves, ui >> increment)
+            increment += direction
+        else
+            if direction == 1
+                direction = -1
+                increment = -1
+            else
+                return moves
+            end
+        end
+    end
+end
+# TODO: check for borders
+
 """
     slidePiece(piece_valid::BitArray{1}, same_color::BitArray{1},
             other_color::BitArray{1}, piece_idx::Int64, increment::Int64)
--- test/test_rooks.jl
+++ test/test_rooks.jl
@@ -45,4 +45,16 @@
 
     rook_mask_dict = Bobby.gen_rook_masks()
     @test mr == rook_mask_dict[Bobby.INT2UINT[45]]
+
+    bb = Bobby.fen_to_bitboard("r1r4k/5ppp/p7/3p4/P1pN1Nb1/2R2nPP/1PP5/4QK2")
+    rook_occupancy = Bobby.ROOK_MASKS[Bobby.INT2UINT[43]] & bb.taken
+    ro = [0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,0,0,0,0,0,0,
+          0,0,1,0,0,0,0,0,
+          0,0,0,0,0,1,1,0,
+          0,0,1,0,0,0,0,0,
+          0,0,0,0,0,0,0,0]
+    @test all(Int.(Bobby.cvt_to_bitarray(rook_occupancy)) .== ro)
 end
