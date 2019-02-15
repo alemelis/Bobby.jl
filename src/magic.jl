2128d1e1db63f1083e131612bcfc90916143d005
Alessandro Melis <alemelis@users.noreply.github.com>
2019-02-15 00:21:26 +0000

    check borders

--- src/magic.jl
+++ src/magic.jl
@@ -1,11 +1,12 @@
 function gen_rank_attacks_from_file_x(file::Int64=1)
     rank_attacks = Dict{UInt64, Array{UInt64,1}}()
+    rank_edges = Dict{UInt64, Array{UInt64,1}}()
 
     for i in 1:8
         pgn = INT2PGN[1+8*(i-1)]
         rank = parse(Int64, pgn[2])
         ui = PGN2UINT[pgn]
-        
+
         for n in 0:2^6-1
             rank_occupancy_mask_string = ("0"^8)^(i-1)*"0"
             pieces = reverse(bitstring(UInt64(n))[58:end])
@@ -26,12 +27,15 @@ function gen_rank_attacks_from_file_x(file::Int64=1)
 
             rank_occupancy_mask = cvt_to_uint(rank_occupancy_mask_string)
             sr = slide_rank(rank_occupancy_mask, ui >> (file-1), MASK_RANKS[rank])
-            if ~isempty(sr)
-                rank_attacks[rank_occupancy_mask] = sr
+            if ~isempty(sr[1])
+                push!(rank_attacks, rank_occupancy_mask => sr[1])
+            end
+            if ~isempty(sr[2])
+                push!(rank_edges, rank_occupancy_mask => sr[2])
             end
         end
     end
-    return rank_attacks
+    return (rank_attacks, rank_edges)
 end
 RANK_ATTACKS_FILE_A = gen_rank_attacks_from_file_x()
 
@@ -39,23 +43,33 @@ function slide_rank(rank_occupancy_mask::UInt64, ui::UInt64, rank_mask::UInt64)
     direction = 1
     increment = 1
     moves = zeros(UInt64, 0)
+    edges = zeros(UInt64, 0)
     while true
-        if (((ui >> increment) & rank_mask) != EMPTY &&
-            rank_occupancy_mask & ((ui >> increment) & rank_mask) == EMPTY)
+        if ((ui >> increment) & rank_mask) != EMPTY
+            if rank_occupancy_mask & ((ui >> increment) & rank_mask) == EMPTY
 
-            push!(moves, ui >> increment)
-            increment += direction
+                push!(moves, ui >> increment)
+                increment += direction
+            else
+                push!(edges, ui >> increment)
+
+                if direction == 1
+                    direction = -1
+                    increment = -1
+                else
+                    return (moves, edges)
+                end
+            end
         else
             if direction == 1
                 direction = -1
                 increment = -1
             else
-                return moves
+                return (moves, edges)
             end
         end
     end
 end
-# TODO: check for borders
 
 #----
 
