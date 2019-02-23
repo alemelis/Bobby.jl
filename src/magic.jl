2e5c3e4d3c70b10573d0a73988f5fedc6c6ed10d
Alessandro Melis <alemelis@users.noreply.github.com>
2019-02-23 23:33:08 +0000

    cross, orthogonal, and start attacks

--- src/magic.jl
+++ src/magic.jl
@@ -1,3 +1,127 @@
+function slide_diagonal(board::UInt64, diag_mask::UInt64, ui::UInt64,
+    compass_direction::String="NW")
+
+    if compass_direction == "NW"
+        increment_mod = 9
+    else
+        increment_mod = 7
+    end
+    direction = 1
+    increment = direction*increment_mod
+
+    moves = zeros(UInt64, 0)
+    edges = zeros(UInt64, 0)
+
+    while true
+        if ((ui >> increment) & diag_mask) != EMPTY # we are on the diagonal
+            if (board & ((ui>>increment) & diag_mask) == EMPTY &&
+                ((ui>>increment) & diag_mask) != EMPTY)
+
+                push!(moves, ui>>increment)
+                increment += direction*increment_mod
+            else
+                push!(edges, ui>>increment)
+
+                if direction == 1
+                    direction = -1
+                    increment = increment_mod*direction
+                else
+                    return moves, edges
+                end
+            end
+        else
+            if direction == 1
+                direction = -1
+                increment = increment_mod*direction
+            else
+                return moves, edges
+            end
+        end
+    end
+end
+
+
+function diagonal_attack(board::UInt64, ui::UInt64)
+    for i in 1:15
+        if DIAGONALS[i] & ui != EMPTY
+            return slide_diagonal(board, DIAGONALS[i], ui, "NW")
+        end
+    end
+end
+
+function antidiagonal_attack(board::UInt64, ui::UInt64)
+    for i in 1:15
+        if ANTIDIAGONALS[i] & ui != EMPTY
+            return slide_diagonal(board, ANTIDIAGONALS[i], ui, "NE")
+        end
+    end
+end
+
+
+function cross_attack(board::UInt64, ui::UInt64)
+    moves, edges = diagonal_attack(board, ui)
+    anti_moves, anti_edges = antidiagonal_attack(board, ui)
+
+    for move in anti_moves
+        push!(moves, move)
+    end
+
+    for edge in anti_edges
+        push!(edges, edge)
+    end
+
+    return moves, edges
+end
+
+
+
+function slide_rank(rank_occupancy_mask::UInt64, ui::UInt64, shift::Int64)
+    direction = 1
+    increment = 1
+    moves = zeros(UInt64, 0)
+    edges = zeros(UInt64, 0)
+    ui = ui >> shift
+    rank_occupancy_mask = rank_occupancy_mask >> shift
+    while true
+        if ((ui >> increment) & MASK_RANK_1) != EMPTY
+            if (rank_occupancy_mask & ((ui >> increment) & MASK_RANK_1) == EMPTY 
+                && ((ui >> increment) & CLEAR_FILE_A & CLEAR_FILE_H) != EMPTY)
+
+                push!(moves, (ui >> increment) << shift)
+                increment += direction
+            else
+                push!(edges, (ui >> increment) << shift)
+
+                if direction == 1
+                    direction = -1
+                    increment = -1
+                else
+                    return (moves, edges)
+                end
+            end
+        else
+            if direction == 1
+                direction = -1
+                increment = -1
+            else
+                return moves, edges
+            end
+        end
+    end
+end
+
+
+function rank_attack(board::UInt64, ui::UInt64)
+    for i in 1:8
+        if MASK_RANKS[i] & ui != EMPTY
+            occupancy_rank = ROOK_MASKS[ui] & MASK_RANKS[i] & board
+            shift = RANK_SHIFTS[i]
+            return slide_rank(occupancy_rank, ui, shift)
+        end
+    end
+end
+
+
 # https://www.hackersdelight.org/hdcodetxt/transpose8.c.txt
 function transpose_uint(x::UInt64)
     y = (x & 0x8040201008040201) |
@@ -16,54 +140,6 @@ function transpose_uint(x::UInt64)
         (x >> 42) & 0x0000000000008040 |
         (x >> 49) & 0x0000000000000080
     return y
-end
-
-
-function slide_rank(rank_occupancy_mask::UInt64, ui::UInt64, shift::Int64)
-    direction = 1
-    increment = 1
-    moves = zeros(UInt64, 0)
-    edges = zeros(UInt64, 0)
-    ui = ui >> shift
-    rank_occupancy_mask = rank_occupancy_mask >> shift
-    while true
-        if ((ui >> increment) & MASK_RANK_1) != EMPTY
-            if (rank_occupancy_mask & ((ui >> increment) & MASK_RANK_1) == EMPTY 
-                && ((ui >> increment) & CLEAR_FILE_A & CLEAR_FILE_H) != EMPTY)
-            
-                push!(moves, (ui >> increment) << shift)
-                increment += direction
-            else
-                push!(edges, (ui >> increment) << shift)
-
-                if direction == 1
-                    direction = -1
-                    increment = -1
-                else
-                    return (moves, edges)
-                end
-            end
-        else
-            if direction == 1
-                direction = -1
-                increment = -1
-            else
-                return (moves, edges)
-            end
-        end
-    end
-end
-
-
-function rank_attack(board::UInt64, ui::UInt64)
-    for i in 1:8
-        if MASK_RANKS[i] & ui != EMPTY
-            occupancy_rank = ROOK_MASKS[ui] & MASK_RANKS[i] & board
-            shift = RANK_SHIFTS[i]
-            mvs, edgs = slide_rank(occupancy_rank, ui, shift)
-            return mvs, edgs
-        end
-    end
 end
 
 
@@ -103,6 +179,21 @@ function orthogonal_attack(board::UInt64, ui::UInt64)
 
 end
 
+
+function star_attack(board::UInt64, ui::UInt64)
+    ortho_moves, ortho_edges = orthogonal_attack(board, ui)
+    cross_moves, cross_edges = cross_attack(board, ui)
+
+    for m in cross_moves
+        push!(ortho_moves, m)
+    end
+    for e in cross_edges
+        push!(ortho_edges, e)
+    end
+    return ortho_moves, ortho_edges
+
+end
+
 #----
 
 """
@@ -138,6 +229,9 @@ function slidePiece(piece_valid::BitArray{1}, same_color::BitArray{1},
     end
     return piece_valid
 end
+
+
+
 
 
 """
