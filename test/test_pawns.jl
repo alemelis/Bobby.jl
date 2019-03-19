0a60b842c772947c0eabc7683cf5745be3b89e5c
Alessandro Melis <alemelis@users.noreply.github.com>
2019-03-19 18:38:06 +0000

    update pawns tests

--- test/test_pawns.jl
+++ test/test_pawns.jl
@@ -1,8 +1,8 @@
 @testset "pawns" begin
-    # bb = Bobby.set_board()
-    # pvl = Bobby.get_pawns_valid_list(bb)
-    # @test length(pvl) == 16
+    bb = Bobby.set_board()
+    pvl = Bobby.get_pawns_list(bb)
+    @test length(pvl) == 16
 
-    # pvl = Bobby.get_pawns_valid_list(bb, "black")
-    # @test length(pvl) == 16
+    pvl = Bobby.get_pawns_list(bb, "black")
+    @test length(pvl) == 16
 end
