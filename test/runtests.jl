ce48714bb1daaf63029965f324b9912d0ee54d70
Alessandro Melis <alemelis@users.noreply.github.com>
2019-04-28 15:17:35 +0100

    new perft

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,41 +11,41 @@ function test_fen(fen, result)
     end
 end
 
-# @testset "parser.jl" begin
-#     include("test_parser.jl")
-# end
+@testset "parser.jl" begin
+    include("test_parser.jl")
+end
 
-# @testset "converters.jl" begin
-#     include("test_converters.jl")
-# end
+@testset "converters.jl" begin
+    include("test_converters.jl")
+end
 
-# @testset "nights.jl" begin
-#     include("test_nights.jl")
-# end
+@testset "nights.jl" begin
+    include("test_nights.jl")
+end
 
-# @testset "king.jl" begin
-#     include("test_king.jl")
-# end
+@testset "king.jl" begin
+    include("test_king.jl")
+end
 
-# @testset "rooks.jl" begin
-#     include("test_rooks.jl")
-# end
+@testset "rooks.jl" begin
+    include("test_rooks.jl")
+end
 
-# @testset "bishops.jl" begin
-#     include("test_bishops.jl")
-# end
+@testset "bishops.jl" begin
+    include("test_bishops.jl")
+end
 
-# @testset "queen.jl" begin
-#     include("test_queen.jl")
-# end
+@testset "queen.jl" begin
+    include("test_queen.jl")
+end
 
-# @testset "pawns.jl" begin
-#     include("test_pawns.jl")
-# end
+@testset "pawns.jl" begin
+    include("test_pawns.jl")
+end
 
-# @testset "check.jl" begin
-#     include("test_check.jl")
-# end
+@testset "check.jl" begin
+    include("test_check.jl")
+end
 
 # @testset "perft.jl" begin
 #     include("test_perft.jl")
